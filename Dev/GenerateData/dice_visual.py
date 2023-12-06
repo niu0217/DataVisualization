c5e189490cbe598737e2a27df25ba99407ac76c1
niu0217 <1033856427@qq.com>
2023-12-06 16:42:20 +0800

    完成生成数据的学习

--- Dev/GenerateData/dice_visual.py
+++ Dev/GenerateData/dice_visual.py
@@ -2,25 +2,30 @@ import plotly.express as px
 
 from die import Die
 
-# 创建一个die实例
-die = Die()
+# 创建两个die实例
+die_1 = Die()
+die_2 = Die()
 
 # 掷几次骰子并将结果存储在一个列表中
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # 分析结果
 frequencies = []
-poss_results = range(1, die.num_sides + 1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result + 1)
 for value in poss_results:
     frequence = results.count(value)
     frequencies.append(frequence)
 
 # 对结果进行可视化
-title = "Results of Rolling One D6 1,000 Times"
+title = "Results of Rolling Two D6 1,000 Times"
 labels = {'x': 'Result', 'y': 'Frequency of Result'}
 fig = px.bar(
     x=poss_results, y=frequencies, title=title, labels=labels)
+
+# 进一步定制图形
+fig.update_layout(xaxis_dtick=1)
 fig.show()
